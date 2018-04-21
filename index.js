c54aa09a504a4a9ba18f7350dd6672cac42eee20
Pranay saha <pranay.saha@live.com>
2018-04-21 21:54:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res){
-  res.send('Hello world from Distelli & Docker!');
+  res.send('Sample app with Distelli & Docker!');
 });
 
 app.listen(PORT);
